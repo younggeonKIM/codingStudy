42667989f4591cb9dc45696aaa3f7463f3382fc9
younggeon <young3702@naver.com>
2023-10-14 14:31:54 +0900

    codingTest_knightsTravel(logic, program)_added

---
+++ knightsTravel/src/logic/KnightTour.java
@@ -0,0 +1,68 @@
+package logic;
+public class KnightTour {
+    private final int n;
+
+    public KnightTour(int n) {
+
+        if (n <= 1) {
+            throw new IllegalArgumentException("The grid must be bigger than " + n + "x" + n);
+        }
+
+        this.n = n;
+    }
+
+    // 나이트가 이동할 수 있는 8가지 이동 경로
+    public static final int COL[] = {1, 2, 2, 1, -1, -2, -2, -1, 1};
+    public static final int ROW[] = {2, 1, -1, -2, -2, -1, 1, 2, 2};
+
+    public void knightTour(int r, int c, int cell, int visited[][]) {
+        if (r < 0 || c < 0 || cell < 0) {
+            throw new IllegalArgumentException("The r, c and cell cannot be negative");
+        }
+
+        if (visited == null) {
+            throw new IllegalArgumentException("The visited[][] cannot be null");
+        }
+
+        // 현재 칸을 방문했다고 표시합니다.
+        visited[r][c] = cell;
+
+        // 이미 경로를 찾았습니다.
+        if (cell >= n * n) {
+            print(visited);
+            // 이전 경로로 돌아가며 방문 표시를 취소합니다.
+            visited[r][c] = 0;
+            return;
+        }
+
+        // 가능한 모든 이동 방향을 확인하고 각각의 유효한 이동에 관해 반복합니다.
+        for (int i = 0; i < (ROW.length - 1); i++) {
+            int newR = r + ROW[i];
+            int newC = c + COL[i];
+
+            // 새 위치가 유효하며 방문한 적이 없는 칸인지 확인합니다.
+            if (isValid(newR, newC) && visited[newR][newC] == 0) {
+                knightTour(newR, newC, cell + 1, visited);
+            }
+        }
+
+        // 이전 경로로 돌아가며 방문 표시를 취소합니다.
+        visited[r][c] = 0;
+    }
+
+    // (r, c)가 유효한 체스판 좌표인지 확인합니다.
+    private boolean isValid(int r, int c) {
+        return !(r < 0 || c < 0 || r >= n || c >= n);
+    }
+
+    // 결과 경로를 체스판 형식으로 출력합니다.
+    private void print(int[][] visited) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.format("%3s", visited[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
---
+++ knightsTravel/src/logic/Main.java
@@ -0,0 +1,19 @@
+package logic;
+
+public class Main {
+
+	public static final int GRID_SIZE = 5; // 최솟값 5
+	
+	public static void main(String[] args) {
+		
+		KnightTour knightTravel = new KnightTour(GRID_SIZE);
+		
+		// visited[][]에 현재 경로를 저장합니다.
+		int visited[][] = new int[GRID_SIZE][GRID_SIZE];
+		
+		int cell = 1;
+		
+		// 구석의 칸 (0, 0)에서 나이트 투어를 시작합니다.
+		knightTravel.knightTour(0, 0, cell, visited);
+	}
+}
